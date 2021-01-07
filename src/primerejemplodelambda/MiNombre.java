652d444c1d75b8ab2e7720f35a427a3225f4c20d
MAC <miguetheking53@gmail.com>
2021-01-07 16:13:36 -0600

    Agregando etiqueta FunctionalInterface a las interfaces

--- src/primerejemplodelambda/MiNombre.java
+++ src/primerejemplodelambda/MiNombre.java
@@ -2,9 +2,11 @@ package primerejemplodelambda;
 
 /**
  * MiNombre Interface.
+ * Representa una interface funcional, ya que cuenta con un unico método abstracto.
  * @author Miguel
  *
  */
+@FunctionalInterface
 public interface MiNombre {
 	
 	/**
--- src/primerejemplodelambda/Sumar.java
+++ src/primerejemplodelambda/Sumar.java
@@ -2,9 +2,11 @@ package primerejemplodelambda;
 
 /**
  * Interface Sumar.
+ * Representa una interface funcional, ya que cuenta con un unico método abstracto.
  * @author Miguel
  *
  */
+@FunctionalInterface
 public interface Sumar {
 	
 	int suma(int a, int b);
