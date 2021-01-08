8cf83b97b2c73f252a10ec325de812bf7486d1c6
MAC <miguetheking53@gmail.com>
2021-01-08 12:58:50 -0600

    Optional or Else

--- src/optional/OptionalPrueba.java
+++ src/optional/OptionalPrueba.java
@@ -11,7 +11,8 @@ import java.util.Optional;
 public class OptionalPrueba {
 
 	public static void main(String[] args) {
-		probarOptional(null);
+		// probarOptional(null);
+		orElseOptional(null);
 	}
 
 	/**
@@ -22,9 +23,24 @@ public class OptionalPrueba {
 	public static void probarOptional(String nombre) {
 		System.out.println(nombre.length());
 	}
-	
+
+	/**
+	 * Crea un Optional.
+	 */
 	public static void crearOptional() {
 		Optional<String> optional = Optional.empty();
+		optional.get();
+	}
+
+	public static void orElseOptional(String nombre) {
+		Optional<String> optional = Optional.ofNullable(nombre);// Acepta un valor nulo en la variable, para despues
+																// asignarle un por defecto.
+		// Optional<String> optional2 = Optional.of(nombre);
+
+		String nombreOfNullable = optional.orElse("Vacio");
+		// String nombreOf = optional2.orElse("Vacio");
+		System.out.println(nombreOfNullable);
+		// System.out.println(nombreOf);
 	}
 
 }
