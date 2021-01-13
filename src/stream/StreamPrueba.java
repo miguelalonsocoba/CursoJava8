d441a09593b4d833817dd7a8e09e59f5e1e6b950
MAC <miguetheking53@gmail.com>
2021-01-12 21:02:50 -0600

    AllMatch, AnyMatch, NoneMatch

--- src/stream/StreamPrueba.java
+++ src/stream/StreamPrueba.java
@@ -59,69 +59,68 @@ public class StreamPrueba {
 		// FlatMap.
 		System.out.println("------------FlatNap--------------");
 		List<List<String>> nombreVariasListas = new ArrayList<List<String>>(
-				Arrays.asList(
-						new ArrayList<String>(Arrays.asList("Alberto", "Maria", "Pedro")),
+				Arrays.asList(new ArrayList<String>(Arrays.asList("Alberto", "Maria", "Pedro")),
 						new ArrayList<String>(Arrays.asList("Monica", "Pablo"))));
-		
-		List<String> nombreUnicaLista = nombreVariasListas.stream()
-				.flatMap((lis) -> lis.stream())
+
+		List<String> nombreUnicaLista = nombreVariasListas.stream().flatMap((lis) -> lis.stream())
 				.collect(Collectors.toList());
 		nombreUnicaLista.stream().forEach((nombre) -> System.out.println(String.format("Nombre: %s", nombre)));
-		
+
 		// Peek
 		System.out.println("-----------------------PEEK---------------------");
 		setUpUser();
-		List<User> users2 = users.stream()
-				.peek((usuario) -> usuario.setNombre(usuario.getNombre() + " Apellido")).collect(Collectors.toList());
-		
+		List<User> users2 = users.stream().peek((usuario) -> usuario.setNombre(usuario.getNombre() + " Apellido"))
+				.collect(Collectors.toList());
+
 		users2.stream().forEach((us) -> System.out.println(String.format("Nombre: %s", us.getNombre())));
-		
+
 		// Count
 		System.out.println("------------------Count---------------------");
 		setUpUser();
-		Long numeroFiltrado = users.stream()
-				.filter((e) -> e.getId() < 3)
-				.count();
+		Long numeroFiltrado = users.stream().filter((e) -> e.getId() < 3).count();
 		System.out.println(String.format("Número filtrado: %s", numeroFiltrado));
-		
+
 		// Skip and Limit.
 		System.out.println("-----------------------Skip and Limit-----------------------");
-		String[] abc  = {"a", "b", "c", "d", "f", "g", "h", "i", "j"};
-		List<String> abcFiltrado = Arrays.stream(abc)
-				.skip(2)
-				.limit(4)
-				.collect(Collectors.toList());
+		String[] abc = { "a", "b", "c", "d", "f", "g", "h", "i", "j" };
+		List<String> abcFiltrado = Arrays.stream(abc).skip(2).limit(4).collect(Collectors.toList());
 		abcFiltrado.stream().forEach((e) -> System.out.println(String.format("ABC Fitrado: %s", e)));
-		
+
 		// Sorted
 		System.out.println("-----------------------------Sorted-----------------------------");
 		setUpUser();
-		List<User> usersSorted = users.stream()
-		.sorted(Comparator.comparing(User::getNombre))
-		.collect(Collectors.toList());
+		List<User> usersSorted = users.stream().sorted(Comparator.comparing(User::getNombre))
+				.collect(Collectors.toList());
 		usersSorted.stream().forEach((e) -> System.out.println(String.format("Nombre: %s", e.getNombre())));
-		
+
 		// Min y Max.
 		System.out.println("--------------------Min y Max-----------------");
 		setUpUser();
-		User userMin = users.stream()
-				.min(Comparator.comparing(User::getId))
-				.orElse(null);
+		User userMin = users.stream().min(Comparator.comparing(User::getId)).orElse(null);
 		System.out.println(String.format("Usuario minimo: %s", userMin));
-		
-		User userMax = users.stream()
-				.max(Comparator.comparing(User::getId))
-				.orElse(null);
+
+		User userMax = users.stream().max(Comparator.comparing(User::getId)).orElse(null);
 		System.out.println(String.format("Usuario maximo: %s", userMax));
-		
+
 		// Distinct.
 		System.out.println("----------------------Distinct----------------------");
-		String[] abc1  = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "a", "c"};
-		List<String> abcFilter1 = Arrays.stream(abc1)
-				.distinct().collect(Collectors.toList());
+		String[] abc1 = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "a", "c" };
+		List<String> abcFilter1 = Arrays.stream(abc1).distinct().collect(Collectors.toList());
 		abcFilter1.stream().forEach((e) -> System.out.println(String.format("Elemento: %s", e)));
-		
-		
+
+		// allMatch, anyMatch, noneMatch.
+		System.out.println("--------------allMatch, anyMatch, noneMatch.----------------------");
+		List<Integer> numberList = Arrays.asList(100, 300, 900, 5000);
+
+		Boolean allMatch = numberList.stream().allMatch((e) -> e > 301);
+		System.out.println(String.format("AllMatch Value: %s", allMatch));
+
+		Boolean anyMatch = numberList.stream().anyMatch((e) -> e > 301);
+		System.out.println(String.format("AnyMatch value: %s", anyMatch));
+
+		Boolean noneMatch = numberList.stream().noneMatch((e) -> e > 4000);
+		System.out.println(String.format("NoneMatch Value; %s", noneMatch));
+
 		stream.close();
 	}
 
