835c25211655f69ea659c99cafcc9bb3949a5455
MAC <miguetheking53@gmail.com>
2021-01-15 22:34:05 -0600

    Implementación de High Order Function

--- src/funcionesdealtoorden/HighOrderFunctions.java
+++ src/funcionesdealtoorden/HighOrderFunctions.java
@@ -2,30 +2,50 @@ package funcionesdealtoorden;
 
 /**
  * Class HighOrderFunctions.
+ * 
  * @author Miguel
  *
  */
 public class HighOrderFunctions implements SumarInterfaz {
-	
+
 	/**
 	 * Método principal.
+	 * 
 	 * @param mac
 	 */
-	public static void main (String... mac) {
+	public static void main(String... mac) {
 		HighOrderFunctions hof = new HighOrderFunctions();
-		
+
+		// --------------------Función-----------------------
 		System.out.println(String.format("Valor de la suma, suma: %s", hof.suma(2, 3)));
+
+		// -------------------Interfaz----------------------
 		System.out.println(String.format("Valor de la suma, apply: %s ", hof.apply(2, 4)));
+
+		// -------------------High Order Function----------
+		SumarInterfaz sumarInterfaz = new SumarInterfaz() {
+
+			@Override
+			public Integer apply(int a, int b) {
+				return a + b;
+			}
+		};
+		System.out.println(String.format("Valor de la suma de la función de alto orden: %s",
+				hof.sumarHighOrderFunction(sumarInterfaz, 5, 6)));
 	}
-	
+
 	public Integer suma(int a, int b) {
 		return a + b;
 	}
 
 	@Override
 	public Integer apply(int a, int b) {
-		
+
 		return a + b;
 	}
-  
+
+	public Integer sumarHighOrderFunction(SumarInterfaz sumar, int a, int b) {
+		return sumar.apply(a, b);
+	}
+
 }
